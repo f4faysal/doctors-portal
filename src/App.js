2beeccd9990ae2c190dec159f1048965c0525eae
f4faysal <f4faysals@gmai.com.com>
2022-11-13 12:07:15 +0600

    add a manu item

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import router from "./Routes/Routes/Routes";
 
 function App() {
   return (
-    <div className="App">
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+    <div className="max-w-[1440px] mx-auto">
+    <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='mx-5'>
+            Home
+        </div>
+    );
+};
+
+export default Home;
