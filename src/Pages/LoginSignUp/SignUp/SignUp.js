ac234cb96a09b79c016f757565512903ae8a69ed
f4faysal <f4faysals@gmai.com.com>
2022-11-14 22:15:53 +0600

    add a fier base

--- src/Pages/LoginSignUp/SignUp/SignUp.js
+++ src/Pages/LoginSignUp/SignUp/SignUp.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Login = () => {
+const SignUp = () => {
     return (
         <div>
-            Login
+            SignUp
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,7 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Appointment from "../../Pages/Appointment/Appointment/Appointment";
 import Home from "../../Pages/Home/Home/Home";
-import Login from "../../Pages/Login/Login";
+import Login from "../../Pages/LoginSignUp/Login/Login";
+import SignUp from "../../Pages/LoginSignUp/SignUp/SignUp";
+
 
 const router = createBrowserRouter([
   {
@@ -16,6 +18,10 @@ const router = createBrowserRouter([
       {
         path : '/login',
          element : <Login></Login>
+      },
+      {
+        path : '/signup',
+         element : <SignUp></SignUp>
       },
       {
         path : '/appointment',
