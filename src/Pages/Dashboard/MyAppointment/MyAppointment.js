8bc10b7965680cc10ba71c41a1b50ebdf78fa0c6
faysal hossain <faysal15-14743@diu.edu.bdy 			+extext??!.EXIT>
2022-11-21 02:49:20 +0600

    zx

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -9,15 +9,18 @@ const MyAppointment = () => {
   const { data: bookings = [] } = useQuery({
     queryKey: ["bookings", user?.email],
     queryFn: async () => {
-
-      // try cghat function handel to error 
-      const res = await fetch(url, {
-        headers: {
-          authorization: `bearer ${localStorage.getItem("accessToken")}`,
-        },
-      });
-      const data = await res.json();
-      return data;
+      try {
+        // try cghat function handel to error 
+        const res = await fetch(url, {
+          headers: {
+            authorization: `bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        const data = await res.json();
+        return data;
+      } catch (error) {
+        console.log("errorerror" , error);
+      }
     },
   });
   return (
@@ -35,7 +38,7 @@ const MyAppointment = () => {
             </tr>
           </thead>
           <tbody>
-            {bookings?.map((booking, i) => (
+            {user.email && bookings?.map((booking, i) => (
               <tr key={booking._id}>
                 <th>{i + 1}</th>
                 <td>{booking.patient}</td>
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -4,6 +4,7 @@ import Main from "../../Layout/Main";
 import Appointment from "../../Pages/Appointment/Appointment/Appointment";
 import AddDoctor from "../../Pages/Dashboard/AddDoctor/AddDoctor";
 import AllUsers from "../../Pages/Dashboard/AllUsers/AllUsers";
+import ManageDoctors from "../../Pages/Dashboard/ManageDoctors/ManageDoctors";
 import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/LoginSignUp/Login/Login";
@@ -55,6 +56,10 @@ const router = createBrowserRouter([
       {
         path: '/dashboard/adddoctor',
         element: <AdminRoute><AddDoctor></AddDoctor></AdminRoute>
+    },
+      {
+        path: '/dashboard/managedoctors',
+        element: <AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
     },
     ],
   },
